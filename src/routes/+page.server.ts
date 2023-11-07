a55caeae4264fbdc27e85fbffac37fafe5cc465a
Eric Egli <43848365+eegli@users.noreply.github.com>
2023-11-07 10:52:38 +0100

    wip

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,32 @@
+import { Octokit } from 'octokit';
+import type { PageServerLoad } from './$types';
+import type { Repository } from '@octokit/graphql-schema';
+
+const octokit = new Octokit({ auth: import.meta.env.VITE_GH_AT });
+
+const query = `
+  query ($owner: String!, $name: String!) {
+    repository(owner: $owner, name: $name) {
+      discussions(first: 10) {
+        nodes {
+          title
+          url
+          bodyHTML
+          author {
+            login
+          }
+          createdAt
+        }
+      }
+    }
+  }
+`;
+
+export const load: PageServerLoad = async (request) => {
+	const { repository } = await octokit.graphql<{ repository: Repository }>(query, {
+		owner: 'eegli',
+		name: 'me'
+	});
+
+	return repository.discussions;
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import type { PageServerData } from './$types';
+	import hljs from 'highlight.js';
+
+	export let data: PageServerData;
+	let html: string = data.nodes![0]!.bodyHTML;
+
+	$: post = data.nodes![0];
+</script>
+
+<main class="max-w-96">
+	<article class="prose lg:prose-xl m-auto">
+		<h1>{post?.title}</h1>
+		<div>{@html html}</div>
+	</article>
+</main>
